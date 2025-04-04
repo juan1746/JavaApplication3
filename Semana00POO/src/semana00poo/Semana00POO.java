7e6776af9635098c2bda95ab5aa75afdfb71a1e2
Alex Fernando Torres Chahuara <miandroidea@gmail.com>
2025-04-03 19:18:55 -0500

    initial

---
+++ Semana00POO/src/semana00poo/Semana00POO.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package semana00poo;
+
+/**
+ *
+ * @author Artika
+ */
+public class Semana00POO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
